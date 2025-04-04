1a909923cfd27780a2d1a1c2766834ec305515d0
Iswar meher <iswarmeher2003@gmail.com>
2025-04-04 16:57:10 +0530

    py modified-->train-test-split

--- main.py
+++ main.py
@@ -10,5 +10,10 @@ file_path="ML Rock or mine\\Data.csv";
 sonar_data=pd.read_csv(file_path,header=None)
 #print(sonar_data.head)
 #print(sonar_data.value_counts()) | m-->mine r-->rock|
-s=sonar_data.groupby(60).mean
-print(s)
+x=sonar_data.drop(columns=60, axis=1)
+y=sonar_data[60]
+
+#train test split
+X_train,X_test,Y_train,Y_test=train_test_split(x,y,test_size=0.1,stratify=y,random_state=1)
+print(x.shape)
+
